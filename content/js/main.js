8b2257050744eeaed276fd475fb38b33a3c01e4b
islamnashaat03 <islamnashaat03@gmail.com>
2021-11-25 22:28:52 +0200

    add latest events section

--- content/js/main.js
+++ content/js/main.js
@@ -32,8 +32,46 @@ window.onload = function () {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
-    })
-  }
+    });
+  };
 
   // END SCROLL TO TOP BUTTON
-}
+
+  // START SKILLS SCROLl
+  let ourSkills = document.getElementById("ourSkills");
+  let spans = document.querySelectorAll(".the-progress");
+
+  window.onscroll = function () {
+    if (window.scrollY >= ourSkills.offsetTop - 300) {
+      spans.forEach((span) => {
+        span.style.width = span.dataset.width;
+      });
+    }
+  };
+  // END SKILLS SCROLL
+  // START TIMER
+  let countDownDate = new Date("dec 25 ,2021").getTime();
+  let counter = setInterval(() => {
+    let dateNow = new Date().getTime();
+    let dateDiff = countDownDate - dateNow;
+    let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+    document.querySelector(".days").innerHTML = days < 10 ? `0${days} ` : days;
+    document.querySelector(".hours").innerHTML =
+      hours < 10 ? `0${hours} ` : hours;
+    document.querySelector(".minutes").innerHTML =
+      minutes < 10 ? `0${minutes} ` : minutes;
+    document.querySelector(".seconds").innerHTML =
+      seconds < 10 ? `0${seconds} ` : seconds;
+    if (dateDiff < 0) {
+      clearInterval(counter);
+    }
+  });
+
+  // END TIMER
+};
